940e24a8587603981d0cc6eedfd36c67e8a80a91
Irina Lyanguzova <iralyang@gmail.com>
2014-07-27 20:59:32 +1200

    implementation of the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The module below caches the results of a matrix inverting
 
-## Write a short comment describing this function
+## This function creates an object that enables
+## to store (cache) the result of a matrix inverting
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(y) i <<- y
+        getinverse <- function() i
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function uses the object created above
+## to return a matrix inverted
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) return(m)
+        m <- solve(x$get())
+        x$setinverse(m)
+        return(m)
 }
